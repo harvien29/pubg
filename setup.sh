0919979394123415f67aac063e0d762e6d2aa9df
Harvien Ocholl <harvien29@gmail.com>
2020-05-06 10:38:01 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,6 @@ time apt install ruby pv toilet tsu git wget screenfetch figlet -y
 #Install LOLCat via Ruby's package manager
 gem install lolcat
 
-#Remove existing files
-#su -c 'find . -iname '*Harvieno*' -exec rm -rf {} \;'
-
 #Fetch the script and setup
 wget https://raw.githubusercontent.com/harvien29/pubg/master/Harvieno -O ./Harvieno
 tsu -c chmod a+x Harvieno
