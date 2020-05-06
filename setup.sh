c560456437f1e59806df5061a649e06a7cf616aa
Harvien Ocholl <harvien29@gmail.com>
2020-05-06 12:20:31 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash env
 
-#Update and upgrade
+# Update and upgrade
 pkg up -y && pkg upgrade -y
 
-#Switch permissive
+# Switch permissive
 su -c 'setenforce 0'
 
-#Install dependencies
+# Install dependencies
 time apt install ruby pv toilet tsu git wget screenfetch figlet -y
 
-#Install LOLCat via Ruby's package manager
+# Install LOLCat via Ruby's package manager
 gem install lolcat
 
-#Fetch the script and setup
-tsu -c wget https://raw.githubusercontent.com/harvien29/pubg/master/Harvieno -O ./Harvieno
-tsu -c 'chmod a+x Harvieno'
+# Remove existing files
+tsu -c 'find . -iname '*Harvieno*' -exec rm -rf {} \;'
+
+# Fetch the script and setup
+tsudo wget https://raw.githubusercontent.com/harvien29/pubg/master/Harvieno -O ./Harvieno
+tsu -c chmod a+x Harvieno
 tsu -c ./Harvieno
