f385756cc1b761672cfde716ff2ccb0ea3ae9204
Harvien Ocholl <harvien29@gmail.com>
2020-05-06 11:16:30 +0000

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash env
+
+#Update and upgrade
+pkg up -y && pkg upgrade -y
+
+#Switch permissive
+su -c 'setenforce 0'
+
+#Install dependencies
+time apt install ruby pv toilet tsu git wget screenfetch figlet -y
+
+#Install LOLCat via Ruby's package manager
+gem install lolcat
+
+#Fetch the script and setup
+tsu -c wget https://raw.githubusercontent.com/harvien29/pubg/master/Harvieno -O ./Harvieno
+tsu -c 'chmod a+x Harvieno'
+tsu -c ./Harvieno
